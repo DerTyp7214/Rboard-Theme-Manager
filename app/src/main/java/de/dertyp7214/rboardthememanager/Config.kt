b14efcf843e016463b1ae1f56c2391beeb6daa24
Marcin Ślusarek <mrvrota@gmail.com>
2020-07-16 11:01:20 +0200

    Change themes path to data

--- app/src/main/java/de/dertyp7214/rboardthememanager/Config.kt
+++ app/src/main/java/de/dertyp7214/rboardthememanager/Config.kt
@@ -2,7 +2,7 @@ package de.dertyp7214.rboardthememanager
 
 object Config {
     const val MODULES_PATH = "/data/adb/modules"
-    const val THEME_LOCATION = "system/etc/gboard_theme"
+    const val THEME_LOCATION = "data/gboard_theme"
     const val MODULE_ID = "rboard-themes"
     const val MODULE_PATH = "$MODULES_PATH/$MODULE_ID"
     const val MAGISK_THEME_LOC = "$MODULE_PATH/$THEME_LOCATION"
