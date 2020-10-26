76f6cb11fcbba0f0783fab368997dc417c100b10
Josua Lengwenath <lengwenath1@gmail.com>
2020-10-26 05:07:25 +0100

    removed unused function

--- app/src/main/java/de/dertyp7214/rboardthememanager/utils/RootUtils.kt
+++ app/src/main/java/de/dertyp7214/rboardthememanager/utils/RootUtils.kt
@@ -1,23 +1,8 @@
 package de.dertyp7214.rboardthememanager.utils
 
 import com.topjohnwu.superuser.Shell
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 object RootUtils {
-    fun checkRoot(): Boolean {
-        var process: Process? = null
-        return try {
-            process = Runtime.getRuntime().exec(arrayOf("/system/xbin/which", "su"))
-            val `in` = BufferedReader(InputStreamReader(process!!.inputStream))
-            `in`.readLine() != null
-        } catch (t: Exception) {
-            false
-        } finally {
-            process?.destroy()
-        }
-    }
-
     fun <E> runWithRoot(run: () -> E): Throwable<E> {
         val triple = newThrowable<E>()
         val throwable = triple.first
